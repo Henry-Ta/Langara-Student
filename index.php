4e6f6531d02ab96ba632c863103718f2025857c7
Henry Ta <hieu.td291294@zoho.com>
2021-01-26 20:51:37 -0800

    feat(html,index.html,js,app.js): +html +index.html +js +app.js refactor(index.php): index.php link to ./html/index.html

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Langara Student</title>
-</head>
-<body>
-    <h1>Welcome to Langara Student Website</h1>
-</body>
-</html>
+<?php
+    include "./html/index.html";
+?>
